842e4eb0544b1edc88b6c3005db32dfdcd9b8e27
SarTore <ericksartori123456@gmail.com>
2022-08-11 22:23:50 -0300

    Primeira V

--- app/src/main/java/br/com/up/sortedodia/MainActivity.java
+++ app/src/main/java/br/com/up/sortedodia/MainActivity.java
@@ -1,14 +1,42 @@
 package br.com.up.sortedodia;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private Button buttonAction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewTitle = findViewById(R.id.textView);
+        buttonAction = findViewById(R.id.button_action);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String[] messages = {"um", "dois", "tres", "quatro"};
+                int position = new Random().nextInt(messages.length);
+                String message = messages[position];
+                textViewTitle.setText(message);
+            }
+        };
+        buttonAction.setOnClickListener(listener);
+
     }
+
 }
